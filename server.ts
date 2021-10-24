0b1043d88045da2baba0feab10c1a5358d50c5ef
CylonicRaider <CylonicRaider@users.noreply.github.com>
2021-10-24 20:07:47 +0200

    Enhance server CLI

--- server.ts
+++ server.ts
@@ -6,19 +6,19 @@ import { join } from 'path';
 import { APP_BASE_HREF } from '@angular/common';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
+import * as minimist from 'minimist';
 
 import { app as api } from './src/api';
 import { AppServerModule } from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
+export function app(distFolder: string): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/needful/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html'))
     ? 'index.original.html'
     : 'index';
 
-  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  // Angular SSR engine.
   server.engine(
     'html',
     ngExpressEngine({
@@ -29,10 +29,10 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', distFolder);
 
-  // Express Rest API endpoints
+  // API endpoints.
   server.all('/api/**', api());
 
-  // Serve static files from /browser
+  // Static files are differentiated by including dots.
   server.get(
     '*.*',
     express.static(distFolder, {
@@ -40,7 +40,7 @@ export function app(): express.Express {
     }),
   );
 
-  // All regular routes use the Universal engine
+  // Everything else is passed to the SSR engine.
   server.get('*', (req, res) => {
     res.render(indexHtml, {
       req,
@@ -51,24 +51,33 @@ export function app(): express.Express {
   return server;
 }
 
-function run(): void {
-  const port = process.env.PORT || 4000;
+function run(argv: string[]): void {
+  const parsed = minimist(argv);
+
+  if (parsed.help) {
+    console.info(`USAGE: server [--host IP] [--port PORT] [--webroot DIR]`);
+    return;
+  }
+
+  const host = parsed.host || process.env.BIND_HOST || '';
+  const port = parsed.post || process.env.PORT || 4000;
+  const statics = parsed.webroot || join(__dirname, '../browser');
 
   // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+  const server = app(statics);
+  server.listen(port, host, () => {
+    console.log(`Serving HTTP on ${host || '*'}:${port}...`);
   });
 }
 
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
+/* Run the server, but only when not require()d. */
+// Webpack will replace 'require' with '__webpack_require__'.
+// '__non_webpack_require__' is a proxy to Node 'require'.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = (mainModule && mainModule.filename) || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-  run();
+  run(process.argv.slice(2));
 }
 
 export * from './src/main.server';
